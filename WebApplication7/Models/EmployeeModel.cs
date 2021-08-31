87f5ac33f19d38a71bb34384180f7db7e3ef9f30
deepa <deepakhagrid1298@gmail.com>
2021-08-31 21:02:36 +0530

    Add project files.

---
+++ WebApplication7/Models/EmployeeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication7.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DOB { get; set; }
+        public string Contact { get; set; }
+        public int RoleId { get; set; }
+
+
+    }
+}
